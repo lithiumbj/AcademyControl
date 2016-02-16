aa3607eab32297b7b6f6f6bd33a423a8da1651b4
Borja José <sat@inforfenix.com>
2016-02-16 13:39:11 -0500

    New invoice design

--- resources/views/invoice/print.blade.php
+++ resources/views/invoice/print.blade.php
@@ -2,140 +2,110 @@
 <!-- Content Header (Page header) -->
 @section('content')
 <?php
+
 use App\Helpers\DateHelper;
 use App\Http\Controllers\RoomController;
 ?>
 <section class="content-header">
-  <ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Factura</a></li>
-    <li class="active">{{$invoice->facnumber}}</li>
-  </ol>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Factura</a></li>
+        <li class="active">{{$invoice->facnumber}}</li>
+    </ol>
 </section>
 
 <!-- Main content -->
 <section class="content">
 
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box box-success">
-        <div class="box-header with-border">
-          <h3 class="box-title">Recibo</h3>
-          <div class="box-tools pull-right">
-            <a href="{{URL::to('/invoice/print/'.$invoice->id)}}" class="btn btn-xs btn-primary"><i class="fa fa-print"></i> Imprimir</a>
-            @if($invoice->status != 2)
-            <a href="{{URL::to('/invoice/pay/'.$invoice->id)}}" class="btn btn-xs btn-success"><i class="fa fa-money"></i> Pagar</a>
-            @else
-              <a href="{{URL::to('/invoice/unpay/'.$invoice->id)}}" class="btn btn-xs btn-warning"><i class="fa fa-trash"></i> Eliminar pago</a>
-            @endif
-            <a href="{{URL::to('/invoice/delete/'.$invoice->id)}}" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Eliminar factura</a>
-          </div>
-        </div><!-- /.box-header -->
-        <div class="box-body">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Recibo -{{$invoice->text_public}}-</h3>
+                    <div class="box-tools pull-right">
+                        @if($invoice->status == 0)
+                        <small class="label bg-default">Borrador</small>
+                        @endif
+                        @if($invoice->status == 1)
+                        <small class="label bg-yellow">Impagada</small>
+                        @endif
+                        @if($invoice->status == 2)
+                        <small class="label bg-green">Pagada</small>
+                        @endif
+                        @if($invoice->status == 3)
+                        <small class="label bg-red">Abandonada / Adeudada</small>
+                        @endif
+                    </div>
+                </div><!-- /.box-header -->
+                <div class="box-body">
 
-            <div class="col-md-6">
-              <table class="table table-bordered">
-                <tr>
-                  <td>Estado</td>
-                  <td>
-                    @if($invoice->status == 0)
-                      <small class="label bg-default">Borrador</small>
-                    @endif
-                    @if($invoice->status == 1)
-                      <small class="label bg-yellow">Impagada</small>
-                    @endif
-                    @if($invoice->status == 2)
-                      <small class="label bg-green">Pagada</small>
-                    @endif
-                    @if($invoice->status == 3)
-                      <small class="label bg-red">Abandonada / Adeudada</small>
-                    @endif
-                  </td>
-                </tr>
-                <tr>
-                  <td>Nota</td>
-                  <td>{{$invoice->text_public}}</td>
-                </tr>
-                <tr>
-                  <td>Fecha de creación</td>
-                  <td>{{date('d/m/Y',strtotime($invoice->date_creation))}}</td>
-                </tr>
-                <tr>
-                  <td>Fecha de pago</td>
-                  @if(count($payments)>0)
-                    <td>{{date('d/m/Y H:i:s',strtotime($payments[count($payments)-1]->created_at))}}</td>
-                  @else
-                    <td>Sin pagos registrados</td>
-                  @endif
-                </tr>
-                <tr>
-                  <td>Importe total</td>
-                  <td><b>{{$invoice->total}}€</b></td>
-                </tr>
-              </table>
-            </div>
-            <!-- Client details -->
-            <div class="col-md-6">
-              <table class="table table-bordered">
-                <tr>
-                  <td style="width:300px;">Nombre</td>
-                  <td>{{$client->name}}</td>
-                </tr>
-                <tr>
-                  <td>Apellidos</td>
-                  <td>{{$client->lastname_1}} {{$client->lastname_2}}</td>
-                </tr>
-              </table>
+                    <div class="col-md-6">
+                        <table class="table table-bordered">
+                            <tr>
+                                <td style="width:300px;"><b>Nombre completo</b></td>
+                                <td><i>{{$client->name}} {{$client->lastname_1}} {{$client->lastname_2}}</i></td>
+                            </tr>
+                            <tr>
+                                <td>Fecha de creación</td>
+                                <td>{{date('d/m/Y',strtotime($invoice->date_creation))}}</td>
+                            </tr>
+                            <tr>
+                                <td>Fecha de pago</td>
+                                @if(count($payments)>0)
+                                <td>{{date('d/m/Y H:i:s',strtotime($payments[count($payments)-1]->created_at))}}</td>
+                                @else
+                                <td>Sin pagos registrados</td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <td>Importe total</td>
+                                <td><b>{{$invoice->total}}€</b></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
     </div>
-  </div>
 
-  <!-- Lines -->
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box box-success">
-        <div class="box-header with-border">
-          <h3 class="box-title">Conceptos del recibo</h3>
-          <div class="box-tools pull-right">
-          </div>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <td>#</td>
-                <td>Producto / Servicio</td>
-                <td>Descripción</td>
-                <td>Base imponible</td>
-                <td>IVA</td>
-                <td>Total</td>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($lines as $line)
-                <tr>
-                  <td>{{$line->fk_service}}</td>
-                  <td>{{$line->prod_name}}</td>
-                  <td>{{$line->prod_description}}</td>
-                  <td>{{$line->tax_base}} €</td>
-                  <td>{{$line->tax}} €</td>
-                  <td>{{$line->tax_base + $line->tax}} €</td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+    <!-- Lines -->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Conceptos del recibo</h3>
+                    <div class="box-tools pull-right">
+                    </div>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td>Producto / Servicio</td>
+                                <td>Descripción</td>
+                                <td>Total</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($lines as $line)
+                            <tr>
+                                <td>{{$line->prod_name}}</td>
+                                <td>{{$line->prod_description}}</td>
+                                <td>{{$line->tax_base + $line->tax}} €</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
 <style>
-.main-header, .main-sidebar, .breadcrumb, .btn, .main-footer{
-  display: none;
-}
-.content-wrapper{
-  margin-left: 0px !important;
-}
+    .main-header, .main-sidebar, .breadcrumb, .btn, .main-footer{
+        display: none;
+    }
+    .content-wrapper{
+        margin-left: 0px !important;
+    }
 </style>
 @stop

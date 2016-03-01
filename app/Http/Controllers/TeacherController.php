0e8082ef1780ae83860e3cc84da4325883f493d4
Borja José <sat@inforfenix.com>
2016-03-01 13:19:25 +0100

    Duplicated rows in client view fixed

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -10,77 +10,91 @@ use App\Models\RoomService;
 use App\Models\RoomReserve;
 use App\Http\Controllers\Controller;
 use App\Helpers\AngularHelper;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 use Validator;
 
-class TeacherController extends Controller
-{
-  /*
-   * Return the actual linked services for the teacher
-   */
-  public static function getServicesForTeacher()
-  {
-    $services = Service::where('fk_teacher', '=', Auth::user()->id)->get();
-    return $services;
-  }
+class TeacherController extends Controller {
+    /*
+     * Return the actual linked services for the teacher
+     */
 
-  /*
-   * This function returns the services for an hour and a day for the service id
-   *
-   */
-  public static function getServicesForHourAndDay($fk_service, $hour, $day)
-  {
-    $services = RoomService::whereRaw('fk_service = '.$fk_service.' AND hour = '.$hour.' AND day = '.$day)->get();
-    return $services;
-  }
+    public static function getServicesForTeacher() {
+        $services = Service::where('fk_teacher', '=', Auth::user()->id)->get();
+        return $services;
+    }
 
-  /*
-   * This function renders the teacher view for a specific group
-   */
-  public function getTeacherView()
-  {
-    //Get the data
-    $data = $_GET;
-    //Get the client list
-    $roomReserves = RoomReserve::whereRaw('fk_room = '.$data['room'].' AND hour = '.$data['hour'].' AND day = '.$data['day'])->get();
-    $clients = [];
-    foreach($roomReserves as $RoomReserve){
-      $tmpArray = [];
-      $tmpArray[] = Client::find($RoomReserve->fk_client);
-      $tmpArray[] = $RoomReserve->id;
-      $clients[] = $tmpArray;
+    /*
+     * This function returns the services for an hour and a day for the service id
+     *
+     */
+
+    public static function getServicesForHourAndDay($fk_service, $hour, $day) {
+        $services = RoomService::whereRaw('fk_service = ' . $fk_service . ' AND hour = ' . $hour . ' AND day = ' . $day)->get();
+        return $services;
     }
-    //Render the view
-    return view('teacher.teach',['clients' => $clients]);
-  }
-  
-  
-  /*
-   * This function returns the amounth of clients for a specific room, hour, and day
-   * 
-   * @param {String} $room - The room id
-   * @param {String} $hour - The room id
-   * @param {String} $day - The room id
-   * 
-   * return {Integer} The quantity of people
-   */
-  public static function getClientsForHour($room, $hour, $day)
-  {
-    //Get the data
-    $data = $_GET;
-    //Get the client list
-    $roomReserves = RoomReserve::whereRaw('fk_room = '.$room.' AND hour = '.$hour.' AND day = '.$day)->get();
-    $clients = [];
-    foreach($roomReserves as $RoomReserve){
-      $tmpArray = [];
-      $tmpArray[] = Client::find($RoomReserve->fk_client);
-      $tmpArray[] = $RoomReserve->id;
-      $clients[] = $tmpArray;
+
+    /*
+     * This function renders the teacher view for a specific group
+     */
+
+    public function getTeacherView() {
+        //Get the data
+        $data = $_GET;
+        //Get the client list
+        $roomReserves = RoomReserve::whereRaw('fk_room = ' . $data['room'] . ' AND hour = ' . $data['hour'] . ' AND day = ' . $data['day'])->get();
+        $clients = [];
+        foreach ($roomReserves as $RoomReserve) {
+            $tmpArray = [];
+            $tmpArray[] = Client::find($RoomReserve->fk_client);
+            $tmpArray[] = $RoomReserve->id;
+            //Check for duplicates
+            if(!$this->checkDuplicatedItems($clients, $tmpArray))
+                $clients[] = $tmpArray;
+        }
+        //Render the view
+        return view('teacher.teach', ['clients' => $clients]);
     }
-    //Render the view
-    return count($clients);
-  }
+
+    /*
+     * Checks for duplicates in the array
+     * 
+     * @param {Array} $array - The data array
+     * @param {Array} $object - The data array (with only one object)
+     */
+
+    public function checkDuplicatedItems($array, $object) {
+        foreach ($array as $item) {
+            if ($object[0]->id == $item[0]->id)
+                return true;
+        }
+        return false;
+    }
+
+    /*
+     * This function returns the amounth of clients for a specific room, hour, and day
+     * 
+     * @param {String} $room - The room id
+     * @param {String} $hour - The room id
+     * @param {String} $day - The room id
+     * 
+     * return {Integer} The quantity of people
+     */
+
+    public static function getClientsForHour($room, $hour, $day) {
+        //Get the data
+        $data = $_GET;
+        //Get the client list
+        $roomReserves = RoomReserve::whereRaw('fk_room = ' . $room . ' AND hour = ' . $hour . ' AND day = ' . $day)->get();
+        $clients = [];
+        foreach ($roomReserves as $RoomReserve) {
+            $tmpArray = [];
+            $tmpArray[] = Client::find($RoomReserve->fk_client);
+            $tmpArray[] = $RoomReserve->id;
+            $clients[] = $tmpArray;
+        }
+        //Render the view
+        return count($clients);
+    }
+
 }
--- app/Models/InvoicePayment.php
+++ app/Models/InvoicePayment.php
@@ -3,10 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 use Auth;
 use DB;
-
 
 /*
  * Detector de duplicados
@@ -14,35 +12,50 @@ use DB;
  * SELECT SUM(invoice.total), invoice_payments.* FROM invoice_payments LEFT JOIN invoice ON invoice.id = invoice_payments.fk_invoice WHERE MONTH(invoice.date_creation) = 1 GROUP BY invoice_payments.fk_invoice;
  * 
  */
-class InvoicePayment extends Model
-{
+
+class InvoicePayment extends Model {
+
     //Table definition
     protected $table = 'invoice_payments';
 
     /*
      * This function returns the current revenue for the current year
      */
-     public static function getMonthlyMoney()
-     {
-       //SELECT MONTH(date_creation) AS mes, SUM(total) AS total FROM `invoice` WHERE status = 1 AND YEAR(date_creation) = "2015" GROUP BY MONTH(date_creation)
+
+    public static function getMonthlyMoney() {
+        //SELECT MONTH(date_creation) AS mes, SUM(total) AS total FROM `invoice` WHERE status = 1 AND YEAR(date_creation) = "2015" GROUP BY MONTH(date_creation)
         $data = DB::table('invoice')
                 ->groupBy(DB::raw('YEAR(date_creation), MONTH(date_creation)'))
-                ->join('invoice_payments','invoice_payments.fk_invoice','=','invoice.id')
-                ->whereRaw('date_creation BETWEEN "'.date('Y-m-d', strtotime(date('Y-m-d')." - 1 year")).'" AND "'.date('Y-m-d').'" AND invoice.fk_company = '.\Auth::user()->fk_company)
+                ->join('invoice_payments', 'invoice_payments.fk_invoice', '=', 'invoice.id')
+                ->whereRaw('date_creation BETWEEN "' . date('Y-m-d', strtotime(date('Y-m-d') . " - 1 year")) . '" AND "' . date('Y-m-d') . '" AND invoice.fk_company = ' . \Auth::user()->fk_company)
                 ->select(DB::raw('MONTH(date_creation) AS month, YEAR(date_creation) AS year, SUM(invoice_payments.total) AS total '))
                 ->get();
-      //Return the data
-      return $data;
-     }
+        //Return the data      
+        if (count($data) > 0){
+            //Return the true data if any
+            return $data;
+        }else{
+            //Return a 0 data if no-any payment has done
+            $fake0 = new \stdClass();
+            $fake0->total = 0;
+            $fake0->month = date('m');
+            //Check if the month are bad-formatted
+            if($fake0->month = 02)
+                    $fake0->month = 2;
+            $fake0->year = date('Y');
+            return [$fake0];
+        }
+    }
 
-     /*
-      * Get's the current due
-      */
-     public static function getDue()
-     {
-       $income = DB::table('invoice_payments')->where("fk_company",'=',Auth::user()->fk_company)->sum('total');
-       $due = DB::table('invoice')->where("fk_company",'=',Auth::user()->fk_company)->sum('total');
-       //Return the data
-       return $due - $income;
-     }
+    /*
+     * Get's the current due
+     */
+
+    public static function getDue() {
+        $income = DB::table('invoice_payments')->where("fk_company", '=', Auth::user()->fk_company)->sum('total');
+        $due = DB::table('invoice')->where("fk_company", '=', Auth::user()->fk_company)->sum('total');
+        //Return the data
+        return $due - $income;
+    }
+
 }
